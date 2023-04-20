0bb0bfcc0bb1dbe9f6805b350126dbaff5c0d9f2
GuilhermeFofano <129764450+GuilhermeFofano@users.noreply.github.com>
2023-04-20 11:40:19 -0300

    Add files via upload

--- ex3.js
+++ ex3.js
@@ -1,14 +1,31 @@
 ({
-    sum : function(component, event, helper){
+    soma : function(component, event, helper){
             var v1 = component.find("txt1").get("v.value");
             var v2 = component.find("txt2").get("v.value");
             var v3 = parseInt(v1)+parseInt(v2);
-            component.set("v.result", v3);
+            component.set("v.result1", v3);
+        component.set("v.actionName", "Seu resultado é: ")
+    },
+    subtracao : function(component, event, helper){
+            var v1 = component.find("txt1").get("v.value");
+            var v2 = component.find("txt2").get("v.value");
+            var v3 = parseInt(v1)-parseInt(v2);
+            component.set("v.result1", v3);
+        	component.set("v.actionName", "Seu resultado é: ")
+    },
+    multiplicacao : function(component, event, helper){
+            var v1 = component.find("txt1").get("v.value");
+            var v2 = component.find("txt2").get("v.value");
+            var v3 = parseInt(v1)*parseInt(v2);
+            component.set("v.result1", v3);
+        	component.set("v.actionName", "Seu resultado é: ")
+    },
+    divisao : function(component, event, helper){
+            var v1 = component.find("txt1").get("v.value");
+            var v2 = component.find("txt2").get("v.value");
+            var v3 = parseInt(v1)/parseInt(v2);
+            component.set("v.result1", v3);
+        	component.set("v.actionName", "Seu resultado é: ")
     }
-
-
-
-
-
 
 })
